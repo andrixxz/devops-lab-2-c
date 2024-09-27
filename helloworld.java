d9d7f84ae387cf1b0e7afae0d52952a9597c8e3a
andrixxz <aluca124@gmail.com>
2024-09-27 12:50:50 +0100

    Changes to the forked java file

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,36 @@
-public class helloworld
-{
-	public static void main(String[] args)
-	{
-	 	System.out.println("Welcome!!");
+import java.util.Scanner;
+
+public class HelloWorld{
+
+	public static void main(String[] args){
+
+		System.out.println("Welcome!!");
+		
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int number = scanner.nextInt();
+
+		int largest = number;
+		int smallest = number;
+
+		for (int i = 1; i < 5; i++){
+		System.out.println("Enter another number: ")
+		number = scanner.nextInt();
+	
+
+		if (number > largest){
+			largest = number;
+		}
+		if (number < smallest){
+			smallest = number;
+		}
 	}
-}
+	
+	System.out.println("Largest number: " + largest);
+	System.out.println("Smallest number" + smallest)
+
+	scanner.close();
+	}
+}		
 
